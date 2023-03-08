d31a838f19092f53ec90e2a16de7e100cfda883b
KopylashTetiana <kopylashtetiana@gmail.com>
2023-03-08 17:04:00 +0200

    class Cat added; DogTest renamed to PetTest; new objects in PetTest created.

--- src/test/java/com/zaurtregulov/spring/DogTest.java
+++
@@ -1,9 +0,0 @@
-package com.zaurtregulov.spring;
-
-public class DogTest {
-    public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.say();
-    }
-
-}
---
+++ src/test/java/com/zaurtregulov/spring/PetTest.java
@@ -0,0 +1,11 @@
+package com.zaurtregulov.spring;
+
+public class PetTest {
+    public static void main(String[] args) {
+        Pet pet = new Dog();
+        pet.say();
+        Pet pet1 = new Cat();
+        pet1.say();
+    }
+
+}
