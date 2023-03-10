d89133bd0a6cff95e4a760b74fce1d011ddb25f7
KopylashTetiana <kopylashtetiana@gmail.com>
2023-03-10 18:13:19 +0200

    class Person and Test3 added;

---
+++ src/test/java/com/tetianacopylash/spring/Test3.java
@@ -0,0 +1,13 @@
+package com.tetianacopylash.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("myPet", Pet.class);
+//        Pet pet = new Cat();
+        Person person = new Person(pet);
+        person.callYourPet();
+    }
+}
