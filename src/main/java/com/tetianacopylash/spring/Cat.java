7c02f8ef87fdbbd53c38c704068272bff73688e6
KopylashTetiana <kopylashtetiana@gmail.com>
2023-03-11 17:14:09 +0200

    Setter into Person added; output messages into constructors Person, Dog, Cat added; <constructor-arg ref> changed to <property name> in applicationContext.xml.

--- src/main/java/com/tetianacopylash/spring/Cat.java
+++ src/main/java/com/tetianacopylash/spring/Cat.java
@@ -1,6 +1,9 @@
 package com.tetianacopylash.spring;
 
 public class Cat implements Pet {
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
     @Override
     public void say() {
         System.out.println("Meow-Meow");
--- src/main/java/com/tetianacopylash/spring/Dog.java
+++ src/main/java/com/tetianacopylash/spring/Dog.java
@@ -1,6 +1,9 @@
 package com.tetianacopylash.spring;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
     @Override
     public void say() {
         System.out.println("Bow-Wow");
