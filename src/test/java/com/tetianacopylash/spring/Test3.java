7e090ed02de4d6b5524e79b7d51f634009a87fc9
KopylashTetiana <kopylashtetiana@gmail.com>
2023-03-11 17:55:34 +0200

    Surname and age added into class Person; <property... value> added into applicationContext.xml.

--- src/test/java/com/tetianacopylash/spring/Test3.java
+++ src/test/java/com/tetianacopylash/spring/Test3.java
@@ -9,6 +9,8 @@ public class Test3 {
 //        Pet pet = new Cat();
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
 
         context.close();
     }
