f2f1f29a00048da22ed1793011101581bf60ec67
KopylashTetiana <kopylashtetiana@gmail.com>
2023-03-13 15:29:16 +0200

    String Dog.name parameter, setName and getName added;operations with myDog.name and yourDog.name added into class Test4. Default attribute scope "singleton" added into applicationContext2.xml.

--- src/main/java/com/tetianacopylash/spring/Dog.java
+++ src/main/java/com/tetianacopylash/spring/Dog.java
@@ -1,9 +1,19 @@
 package com.tetianacopylash.spring;
 
 public class Dog implements Pet {
+    private String name;
     public Dog() {
         System.out.println("Dog bean is created");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public void say() {
         System.out.println("Bow-Wow");
--- src/test/java/com/tetianacopylash/spring/Test4.java
+++ src/test/java/com/tetianacopylash/spring/Test4.java
@@ -6,10 +6,14 @@ public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
         Dog myDog = context.getBean("myPet", Dog.class);
+        myDog.setName("Mikky");
         Dog yourDog = context.getBean("myPet", Dog.class);
-        System.out.println("Do the variables refer on the same object? " + (myDog == yourDog));
-        System.out.println(myDog);
-        System.out.println(yourDog);
+        yourDog.setName("Rokky");
+        System.out.println(myDog.getName());
+        System.out.println(yourDog.getName());
+//        System.out.println("Do the variables refer on the same object? " + (myDog == yourDog));
+//        System.out.println(myDog);
+//        System.out.println(yourDog);
         context.close();
     }
 }
