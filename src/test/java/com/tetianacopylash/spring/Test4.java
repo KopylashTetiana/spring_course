54ed3823b7a945e51f72af049d16147b659a4ee2
KopylashTetiana <kopylashtetiana@gmail.com>
2023-03-13 11:33:47 +0200

    Test4 and applicationContext2.xml created.

---
+++ src/test/java/com/tetianacopylash/spring/Test4.java
@@ -0,0 +1,15 @@
+package com.tetianacopylash.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+        Dog myDog = context.getBean("myPet", Dog.class);
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        System.out.println("Do the variables refer on the same object? " + (myDog == yourDog));
+        System.out.println(myDog);
+        System.out.println(yourDog);
+        context.close();
+    }
+}
