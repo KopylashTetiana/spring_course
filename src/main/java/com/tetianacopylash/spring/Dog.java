6c6227b814b202b14b8b5a7c24a13f110f8a3c9c
KopylashTetiana <kopylashtetiana@gmail.com>
2023-03-16 17:04:48 +0200

    String name removed from Dog class, init() and destroy() method added into Dog; init and destroy added into applicationContext2.xml; scope 'prototype' deleted.

--- src/main/java/com/tetianacopylash/spring/Dog.java
+++ src/main/java/com/tetianacopylash/spring/Dog.java
@@ -1,18 +1,26 @@
 package com.tetianacopylash.spring;
 
 public class Dog implements Pet {
-    private String name;
+//    private String name;
     public Dog() {
         System.out.println("Dog bean is created");
     }
 
-    public String getName() {
-        return name;
+    public void init() {
+        System.out.println("ClassDog: init method");
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void destroy() {
+        System.out.println("Class Dog: destroy method");
     }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     @Override
     public void say() {
--- src/test/java/com/tetianacopylash/spring/Test4.java
+++ src/test/java/com/tetianacopylash/spring/Test4.java
@@ -6,11 +6,11 @@ public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
         Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Mikky");
-        Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Rokky");
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
+//        myDog.setName("Mikky");
+//        Dog yourDog = context.getBean("myPet", Dog.class);
+//        yourDog.setName("Rokky");
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 //        System.out.println("Do the variables refer on the same object? " + (myDog == yourDog));
 //        System.out.println(myDog);
 //        System.out.println(yourDog);
